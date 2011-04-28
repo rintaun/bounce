ca8cae456194cd0844cba102b0134ec642135fa6
Matthew Lanigan <rintaun@gmail.com>
2011-04-28 03:19:44 -0400

    Add LICENSE file and directories

---
+++ bounce.php
@@ -0,0 +1,16 @@
+<?php
+
+/************************************************************
+ * Bounce v0.1-alpha                                        *
+ * Author: rintaun - Matthew J. Lanigan <rintaun@gmail.com> *
+ *                                                          *
+ * Copyright 2011 Matthew J. Lanigan.                       *
+ * See LICENSE file for licensing details.                  *
+ ************************************************************
+ * bounce.php                                               *
+ * Last Modified: 4/28/2011                                 *
+ *                                                          *
+ * Description: Bounce program wrapper                      *
+ ************************************************************/
+
+
